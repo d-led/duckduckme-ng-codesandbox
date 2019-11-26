38cc7b0cf3de95a626dd7c2ecc5a85f7cbe5c0a3
Dmitry Ledentsov <d-led@users.noreply.github.com>
2019-11-26 13:14:34 +0100

    hit enter to search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,10 @@ export class AppComponent {
   name = "Angular";
   results = [];
   constructor(private http: HttpClient) {}
-  searchResults() {
+  searchResults(event = null) {
+    if (event && event.key !== "Enter") {
+      return;
+    }
     return this.http
       .jsonp(
         "https://api.duckduckgo.com/?q=sensor&format=json&pretty=1",
